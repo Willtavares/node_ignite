3c0e501d398d41f0992abc20de176c60d8220570
Willtavares <willtavares@outlook.com>
2022-04-10 22:48:44 -0300

    Delete e check Balance

--- src/index.js
+++ src/index.js
@@ -140,6 +140,20 @@ app.get('/account', verifyExistsAccountCPF, (request, response) => {
   return response.json(customer)
 })
 
+app.delete('/account', verifyExistsAccountCPF, (request, response) => {
+  const {customer} = request;
+
+  customers.splice(customer, 1)
+  return response.status(200).json(customers)
+})
+
+app.get('/balace', verifyExistsAccountCPF, (request, response) => {
+ const {customer} = request;
+
+ const balance = getBalance(customer.statement)
+
+  return response.json(balance)
+})
 //Indicando qual porta eu desejo usar para inicializar o projeto.
 // Neste caso foi usada a porta 3333
 app.listen(3333);
